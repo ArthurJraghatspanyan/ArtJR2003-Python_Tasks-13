8daa969585583ab624fdbfe4331d1d3f25779735
Arthur Jraghatspanyan <arthur@Arthurs-MacBook-Air.local>
2024-07-24 00:47:04 +0400

    forth commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+st1 = {1, 2, 3, 4}
+st2 = {3, 6, 7, 8}
+print(st1 | st2)
+print(st2 & st1)
+print(st1 ^ st2)
---
+++ 2.py
@@ -0,0 +1,34 @@
+#Ստեղծել ծրագիր, որը կներառի օգտատերերի տվյալների ներմուծումը, պահպանումը բառարանում և ցուցակում, ապա ցույց կտա օգտատերերին ըստ ներմուծված անվան(“Not found”, եթե այդպիսի օգտատեր գոյություն չունի)։ Յուրաքանչյուր օգտատեր պետք է ունենա հետևյալ դաշտերը՝ ID, անուն, ազգանուն, Էլ. փոստ, գաղտնաբառ և հեռախոսահամար:
+
+users = {}
+
+count = int(input("Enter count of users: "))
+
+for _ in range(count):
+    user_id = int(input("Enter ID: "))
+    name = input("Enter name: ")
+    surname = input("Enter surname: ")
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    phone_number = input("Enter phone number: ")
+    
+    users[user_id] = {
+        'ID': user_id,
+        'Name': name,
+		'Surname': surname,
+        'Email': email,
+        'Password': password,
+        'Phone Number': phone_number
+    }
+
+search_name = input("Enter name you want to search: ")
+found_user = "Not found"
+for user in users.values():
+    if user['Name'] == search_name:
+        found_user = user
+        break
+
+print(found_user)
+
+for user_id, user_data in users.items():
+    print(f"ID: {user_id}, Name: {user_data['Name']}, Surname: {user_data['Surname']}, Email: {user_data['Email']}, Password: {user_data['Phone Number']}, Phone Number: {user_data['Phone Number']}")
